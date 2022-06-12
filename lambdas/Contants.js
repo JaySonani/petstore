41e2a251278e27849049f557dc24910320529fe6
Jay Bhagvanbhai Sonani <jy652788@dal.ca>
2022-06-12 19:53:33 -0300

    Find order by id

--- lambdas/Contants.js
+++ lambdas/Contants.js
@@ -1,15 +1,21 @@
 const STATUS_IS_REQUIRED = "Status is required"
 const INVALID_INPUT = "Invalid input"
 const INVALID_ID = "Invalid ID supplied"
-const PET_PROPERTIES = ["id", "category", "name", "photoUrls", "tags", "status"]
 const PET_NOT_FOUND = "Pet not found"
+const ORDER_NOT_FOUND = "Order not found"
+const PET_PROPERTIES = ["id", "category", "name", "photoUrls", "tags", "status"]
+const ORDER_PROPERTIES = ["id", "petId", "quantity", "shipDate", "status", "complete"]
+const INVALID_ORDER = "Invalid order"
 
 module.exports = {
     STATUS_IS_REQUIRED,
     INVALID_INPUT,
     INVALID_ID,
     PET_PROPERTIES,
-    PET_NOT_FOUND
+    ORDER_PROPERTIES,
+    PET_NOT_FOUND,
+    INVALID_ORDER,
+    ORDER_NOT_FOUND
 }
 
 
